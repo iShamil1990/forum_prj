57aa1f572558c5e1282db43288e3332d5d66010c
iGizmo <test@example.com>
2022-06-20 07:28:50 +0300

    added login form

---
+++ service/forms.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from .models import Post, Comment
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={'class':'form-control'}),
+            "description": forms.Textarea(attrs={'class':'form-control'}),           
+            }
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['description']
+        widgets = {
+            "description": forms.Textarea(attrs={'class':'form-control'}),           
+            }
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
