a5a618e77b85fd5ccea01451614789b430d338e7
iGizmo <test@example.com>
2022-06-20 23:43:19 +0300

    email

--- service/forms.py
+++ service/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Post, Comment
+from .models import Post, Comment, Message
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -20,6 +20,14 @@ class CommentForm(ModelForm):
         widgets = {
             "description": forms.Textarea(attrs={'class':'form-control'}),           
             }
+        
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['title', 'body']
+        widgets = {
+            "body": forms.Textarea(attrs={'class':'form-control'}),           
+        }
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
--- service/models.py
+++ service/models.py
@@ -27,3 +27,14 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse("index")
+    
+    
+class Message(models.Model):
+    title = models.CharField(verbose_name='Subject', max_length=250, null=True, blank=True)
+    body = models.TextField(verbose_name='Body')
+
+    def __str__(self):
+        return f'{self.body}'
+
+    # def get_absolute_url(self):
+    #     return reverse("index")
