59f62fd2cb1d21d7297831fe2cd685554d496031
iShamil1990 <106529697+iShamil1990@users.noreply.github.com>
2022-08-04 13:06:03 +0300

    Update models.py

--- service/models.py
+++ service/models.py
@@ -4,11 +4,11 @@ from django.urls import reverse
 
 class Post(models.Model):
 
-    title = models.CharField(verbose_name='Название', max_length=100)
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    image = models.ImageField(verbose_name='Картинка', null=True, blank=True)
-    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    title = models.CharField(verbose_name='Title', max_length=100)
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
+    image = models.ImageField(verbose_name='Picture', null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='Date of creation', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Date of change', auto_now=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -18,9 +18,9 @@ class Post(models.Model):
 
 class Comment(models.Model):
 
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
+    post = models.ForeignKey(Post, related_name='Сomments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='Date of creation', auto_now_add=True)
 
     def __str__(self):
         return f'{self.description}'
@@ -35,6 +35,3 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.body}'
-
-    # def get_absolute_url(self):
-    #     return reverse("index")
